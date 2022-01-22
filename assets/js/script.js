46e55af506033f34bfd4fa876f99404627b1eec2
Justin VanHaven <vanhavenjustin@gmail.com>
2022-01-22 15:45:55 -0500

    justinpush

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,24 +11,18 @@ $("#submit").on("click", function (event) {
   var textContent = $(this).siblings("input").val();
   var storeArr = [];
   storeArr.push(textContent);
-  localStorage.setItem('charSearch', JSON.stringify(storeArr))
-
-
+  localStorage.setItem('charSearch', JSON.stringify(storeArr));
   findCharacter(userInput);
+  const origin = "https://en.wikipedia.org";
+  const url = `${origin}/w/api.php?action=query&origin=*&format=json&list=search&srsearch=${userInput}&srlimit=5`
+  fetch(url).then(function (data) {
+    return data.json()
+  
+  }).then(displayData).catch(function (error) {
+    console.log(error);
+
+  })
 });
-
-
-    function getWikiResponse() {
-        var wikiApi = "https://en.wikipedia.org/w/api.php?action=query&origin=*&format=json&generator=search&gsrnamespace=0&gsrlimit=5&gsrsearch=%Wolverine%";
-
-        fetch(wikiApi)
-        .then(function(response) {
-            return response.json();
-        })
-        .then(function (data) {
-            console.log(data);
-        })
-    }
 
 function findCharacter(userInput){
 
@@ -68,4 +62,17 @@ console.log(data);
 });
     };
 
- getWikiResponse();
+function displayData(data) {
+  let result = data.query.search;
+  console.log(result);
+  let output = '';
+  result.forEach(function (item) {
+  output += `<li class="search-item">
+        <h2 class="search-item__title">${item.title}</h2>
+        <p class="search-item__text">${item.snippet}</p>
+        <a href="http://en.wikipedia.org/?curid=${item.pageid}" class="search-item__link main-btn" target="_blank">read more ...</a>
+        </li>`
+      })
+      document.querySelector('.wikiSpot').innerHTML = output;
+     
+     }
