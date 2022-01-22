bc26579072dfbc3206f6e765c73949fa2e012ccf
amartinezjr7 <94876426+amartinezjr7@users.noreply.github.com>
2022-01-22 09:16:15 -0800

    Finally got the search working!!!
    This took forever, but i got the search working, so happy

--- assets/js/script.js
+++ assets/js/script.js
@@ -64,35 +64,30 @@ $(function() {
   } )
 
 
-
-
-// function getApi() {
-// var apiMarvel = "http://gateway.marvel.com/v1/public/comics?ts=1&apikey=14ba62392acaeee875f06ebee23b5c60";
-
-// fetch(apiMarvel)
-//     .then(function(response) {
-//         return response.json();
-//     })
-//     .then(function(data) {
-//         console.log(data);
-//     }
-
-
-// getApi();
-// you will also have to setup the referring domains on your marvel developer portal
-
 var PRIV_KEY = "96b6dcb17afd3a92bf2cb5412a5efad74a978e6d";
 var PUBLIC_KEY = "14ba62392acaeee875f06ebee23b5c60";
-function getMarvelResponse() {
-    // you need a new ts every request
-    
-    var ts = new Date().getTime();  
-    var hash = CryptoJS.MD5(ts + PRIV_KEY + PUBLIC_KEY).toString();    
-    // the api deals a lot in ids rather than just the strings you want to use  
-    var characterId = '1009718'; 
+var url = 'http://gateway.marvel.com/v1/public/comics';
+var ts = new Date().getTime();
+var hash = CryptoJS.MD5(ts + PRIV_KEY + PUBLIC_KEY).toString();
+
+
+$("#submit").on("click", function (event) {
+  event.preventDefault();
+  var userInput = $("#searchChar").val().trim();
+  var textContent = $(this).siblings("input").val();
+  var storeArr = [];
+  storeArr.push(textContent);
+  localStorage.setItem('charSearch', JSON.stringify(storeArr))
+  //console.log(storeArr[0]);
+
+
+  findCharacter(userInput);
+});
+
+function getMarvelResponse() { 
+    var characterId ='1009718'; 
     // wolverine
-    var url = 'http://gateway.marvel.com:80/v1/public/comics';
-    console.log(url);
+    
       $.getJSON(url, {
               ts: ts,
               apikey: PUBLIC_KEY,
@@ -100,15 +95,13 @@ function getMarvelResponse() {
               characters: characterId
                 })
           .done(function(data) {      
-        // sort of a long dump you will need to sort through      
+    
         console.log(data);
         })
-            .fail(function(err){      
-            // the error codes are listed on the dev site      
+            .fail(function(err){            
             console.log(err);    
         });
     };
-
 
     function getWikiResponse() {
         var wikiApi = "https://en.wikipedia.org/w/api.php?action=query&origin=*&format=json&generator=search&gsrnamespace=0&gsrlimit=5&gsrsearch=%Wolverine%";
@@ -122,9 +115,38 @@ function getMarvelResponse() {
         })
     }
 
-   
+function findCharacter(userInput){
 
-  getMarvelResponse();
+    var charURL="http://gateway.marvel.com/v1/public/characters";
+    console.log(userInput);
 
-  getWikiResponse();
+    $.getJSON(charURL, {
+      name: userInput,
+      ts: ts,
+      apikey: PUBLIC_KEY,
+      hash: hash
+      
+        })
+  .done(function(data) { 
+    var charNameDes = document.getElementById('characters');
+    charNameDes.innerHTML=
+    "<div class=block><h2>"+data.data.results[0].name+
+    "<h2><figure><img src="+data.data.results[0].thumbnail.path+"."+data.data.results[0].thumbnail.extension+
+    "></figure><p>"+data.data.results[0].description+"</p>"
+    "<div>";
+
+
+  
+console.log(data);
+console.log(data.data.results[0].thumbnail.path+"."+data.data.results[0].thumbnail.extension);
+})
+    .fail(function(err){            
+    console.log(err);    
+});
+    };
+
+
+  //getMarvelResponse();
+
+ getWikiResponse();
 
